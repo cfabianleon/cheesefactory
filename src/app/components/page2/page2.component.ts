a693f1605233dac0d1caa9d85a96768ab46c2d66
Carlos Fabian Leon Rojas <carlos.fabian.leon@ibm.com>
2021-04-03 17:51:18 -0500

    pruba 1

--- src/app/components/page2/page2.component.ts
+++ src/app/components/page2/page2.component.ts
@@ -13,7 +13,7 @@ export class Page2Component implements OnInit {
   quesos = [
     {id:1,title: 'Paipa', subtitle: 'Subtitle', content: 'Content here', url: 'assets/paipa.PNG'},
     {id:2,title: 'Mozarella', subtitle: 'Subtitle', content: 'Content here', url: 'assets/mozarella.PNG'},
-    {id:3,title: 'Otro', subtitle: 'Subtitle', content: 'Content here', url: '.assets/mozarella.PNG'},
+    {id:3,title: 'Otro', subtitle: 'Subtitle', content: 'Content here', url: 'assets/mozarella.PNG'},
     {id:4,title: 'Paipa', subtitle: 'Subtitle', content: 'Content here', url: 'assets/paipa.PNG'},
     {id:5,title: 'Mozarella', subtitle: 'Subtitle', content: 'Content here', url: 'assets/mozarella.PNG'},
     {id:6,title: 'Otro', subtitle: 'Subtitle', content: 'Content here', url: 'assets/mozarella.PNG'},
